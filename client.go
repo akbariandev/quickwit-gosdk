e0227d1acf73da641b48b156b60df2ae8fa6fff3
mj <akbarian.dev@gmail.com>
2026-07-31 15:38:24 +0330

    handle resty errors using OnAfterResponse

--- client.go
+++ client.go
@@ -1,7 +1,9 @@
 package quickwitgosdk
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/go-resty/resty/v2"
@@ -32,6 +34,25 @@ type Client struct {
 // NewClient creates a new Quickwit client with the given base URL and optional configuration.
 func NewClient(baseURL string, opts ...ClientOption) *Client {
 	httpClient := resty.New().SetBaseURL(baseURL)
+
+	// Treat any non-2xx response as an error. Quickwit returns error details as JSON
+	// in the body (e.g. {"message": "..."}); extract and surface them as QuickwitError.
+	httpClient.OnAfterResponse(func(_ *resty.Client, resp *resty.Response) error {
+		if resp.IsSuccess() {
+			return nil
+		}
+
+		// Try to extract Quickwit's error message from the JSON body.
+		var body struct {
+			Message string `json:"message"`
+		}
+		msg := resp.Status()
+		if err := json.Unmarshal(resp.Body(), &body); err == nil && strings.TrimSpace(body.Message) != "" {
+			msg = body.Message
+		}
+		return &QuickwitError{StatusCode: resp.StatusCode(), Message: msg}
+	})
+
 	c := &Client{client: httpClient}
 
 	for _, opt := range opts {
